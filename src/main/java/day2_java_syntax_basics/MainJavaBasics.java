2bbcf82eb93a2e1bd3b9bc1c95a4a103c0be12a8
Jojo-is-here <indisguise01@gmail.com>
2024-09-29 15:20:27 +0800

    Add exclamation point

--- src/main/java/day2_java_syntax_basics/MainJavaBasics.java
+++ src/main/java/day2_java_syntax_basics/MainJavaBasics.java
@@ -2,6 +2,6 @@ package day2_java_syntax_basics;
 
 public class MainJavaBasics {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World!!!!");
     }
 }
